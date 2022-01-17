470c1abe4912892bf0c9a0d39a2f857edb6e8b18
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-01-17 11:42:57 +0530

    Time: 698 ms (10.00%), Space: 123 MB (40.00%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& q) {
-        vector<long long> dp(2e5+1); 
-        for(int i=q.size()-1; i>=0; i--) {
-            dp[i] = max(q[i][0] + dp[i + q[i][1] + 1], dp[i+1]);
-        }
+    long long solve(int ind, vector<vector<int>>& q, vector<long long>& dp) {
+        if(ind == q.size()-1)
+            return q[ind][0]; 
         
-        return dp[0];
+        if(ind >= q.size()) return 0; 
+        
+        if(dp[ind] != -1) return dp[ind]; 
+        
+        long long solved = q[ind][0] + solve(ind + q[ind][1] + 1, q, dp); 
+        long long skiped = 0 + solve(ind + 1, q, dp);
+        
+        return dp[ind] = max(solved, skiped);
+    }
+    long long mostPoints(vector<vector<int>>& q) {
+        vector<long long> dp(q.size(), -1);
+        return solve(0, q, dp);
     }
 };
