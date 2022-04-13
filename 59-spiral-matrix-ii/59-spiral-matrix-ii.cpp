091e7ae43590fcb555261e9cf34769a927e1200f
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-04-13 08:56:56 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -8,8 +8,8 @@ public:
             for(int i=c1; i<=c2; i++) ans[r1][i] = val++;
             for(int i=r1+1; i<=r2; i++) ans[i][c2] = val++;
             if(r1<r2 and c1<c2) {
-            for(int i=c2-1; i>c1; i--) ans[r2][i] = val++;
-            for(int i=r2; i>r1; i--) ans[i][c1] = val++;
+                for(int i=c2-1; i>c1; i--) ans[r2][i] = val++;
+                for(int i=r2; i>r1; i--) ans[i][c1] = val++;
             }
             
             r1++; r2--; c1++; c2--;
