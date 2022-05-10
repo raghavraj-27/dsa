816ed787d2c68a540d25be04c08b0f3ed95bfcb8
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-05-10 10:23:20 +0530

    Time: 2 ms (70.97%), Space: 9.2 MB (31.70%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    // Gap Strategy
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        for(int j=0; j<n; j++) {
-            nums1[m+j] = nums2[j];
+        for(int i=0; i<n; i++) {
+            nums1[m + i] = nums2[i];
         }
         
-        int gap = ceil((m + n) / 2.0);
+        int gap = ceil((m+n) / 2.0);
         
-        // if((m+n) % 2 == 1)
-        //     gap++; 
-        
-        for(int g=gap; g>0; )
-        {
-            int i=0, j=g; 
-            while(j<nums1.size())
-            {
-                if(nums1[i] > nums1[j])
+        for(int g=gap; g>0; ) {
+            int i = 0, j = g;
+            while(j < nums1.size()) {
+                if(nums1[i] > nums1[j]) {
                     swap(nums1[i], nums1[j]);
+                }
                 i++; j++;
+                
             }
             
-            if(g%2 == 1 and g!=1)
-                g = g/2 + 1;
-            else
-                g = g/2;
+            if(g != 1 and g%2 == 1) g = g/2 + 1;
+            else g = g / 2;
         }
     }
 };
