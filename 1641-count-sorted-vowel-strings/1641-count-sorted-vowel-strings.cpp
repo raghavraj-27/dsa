9095900459cf7567263b264f196cfb108d528bd8
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-05-11 12:52:37 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.14%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<vector<int>> dp(n+1, vector<int> (6, 0));
+        
+        // for n = 1
+        for(int i=1; i<=5; i++) {
+            dp[1][i] = 1;
+        }
+        
+        for(int i=2; i<=n; i++) {
+            int sum = accumulate(dp[i-1].begin(), dp[i-1].end(), 0);
+            int presum = 0;
+            for(int j=1; j<=5; j++) {
+                presum += dp[i-1][j-1];
+                dp[i][j] = sum - presum;
+                
+                
+            }
+        }
+    
+        return accumulate(dp[n].begin(), dp[n].end(), 0);
+    }
+};
