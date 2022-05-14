718aabc11636c1ee902092c9cf70cdcba9044c40
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-05-14 14:30:10 +0530

    Time: 65 ms (27.62%), Space: 18.8 MB (97.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // BRUTE FORCE
+    // BRUTE FORCE TC = O(n log n) + O(n^2), SC = O(1)
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         int n = intervals.size();
@@ -14,7 +14,6 @@ public:
             temp[1] = intervals[i][1];
             
             int j;
-            // cout<<temp[0]<<" "<<temp[1]<<"|";
             for(j=i+1; j<n; j++){
                 if(temp[1]>=intervals[j][0]){
                     if(temp[1]<intervals[j][1]){
@@ -30,4 +29,33 @@ public:
         }
         return ans;
     }
+    
+    // EFFICIENT APPROACH : TC = O(n log n) + O(n), SC = O(1)
+    /*
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        
+        pair<int, int> p;
+        p.first = intervals[0][0]; p.second = intervals[0][1];
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        
+        for(int i=1; i<intervals.size(); i++) {
+            if(p.second >= intervals[i][0]) {
+                p.first = min(intervals[i][0], p.first); 
+                p.second = max(intervals[i][1], p.second); 
+            } else {
+                vector<int> v = {p.first, p.second}; 
+                ans.push_back(v); 
+                p.first = intervals[i][0]; 
+                p.second = intervals[i][1];
+            }
+        }
+        
+        vector<int> v = {p.first, p.second}; 
+        ans.push_back(v); 
+        
+        return ans;
+    }
+    */
 };
