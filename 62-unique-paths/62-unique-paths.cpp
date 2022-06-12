b92d42a922cd8c479b5c5e6b14c372fc8d218689
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-06-12 19:05:54 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (44.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int countUniquePaths(int i, int j, int m, int n, vector<vector<int>> & dp)
-    {
-        if(i<0 or j<0 or i==m or j==n)
-            return 0; 
-        
-        if(i==m-1 and j==n-1)
-            return 1; 
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int left = countUniquePaths(i, j+1, m, n, dp); 
-        int right = countUniquePaths(i+1, j, m, n, dp); 
-        
-        return dp[i][j] = left + right;
-    }
-    
-    int uniquePaths(int m, int n) {
-        if(m==1 and n==1)
-            return 1;
-        
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return countUniquePaths(0, 0, m, n, dp);
-    }
+int countUniquePaths(int i, int j, int m, int n, vector<vector<int>> & dp)
+{
+    if(i<0 or j<0 or i==m or j==n)
+        return 0; 
+
+    if(i==m-1 and j==n-1)
+        return 1; 
+
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    int left = countUniquePaths(i, j+1, m, n, dp); 
+    int right = countUniquePaths(i+1, j, m, n, dp); 
+
+    return dp[i][j] = left + right;
+}
+
+int uniquePaths(int m, int n) {
+    if(m==1 and n==1)
+        return 1;
+
+    vector<vector<int>> dp(m, vector<int> (n, -1));
+    return countUniquePaths(0, 0, m, n, dp);
+}
 };
