381977eab6a28d394d9dafc7d479edda843a1630
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-06-16 11:53:34 +0530

    Time: 1581 ms (5.01%), Space: 30.2 MB (32.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int size = s.size(), min_start=0, max_len=1;
+        vector<vector<bool>> dp(s.size(), vector<bool> (s.size(), false));
+        int start = 0, max_len = 1;
         
-        vector<vector<bool>> dp(size, vector<bool> (size, false));
-
-        for(int i=0; i<size; i++) 
-        {
-            dp[i][i] = true;
-            if(i!=size-1)
-            {
-                dp[i][i+1] = s.at(i)==s.at(i+1);
-                if(dp[i][i+1])
-                {
-                    min_start = i; 
-                    max_len = 2;
+        for(int g=0; g<s.size(); g++) {
+            for(int i=0, j=g; j<s.size(); j++, i++) {
+                if(g==0) {
+                    dp[i][i] = true;
+                } else if(g==1) {
+                    dp[i][j] = (s[i]==s[j]);
+                    // start = i; max_len = 2;
+                } else {
+                    dp[i][j] = (s[i]==s[j]) && dp[i+1][j-1];
+                    
+//                     if(s[i] == s[j]) {
+//                         if(dp[i+1][j-1]) {
+                            
+//                             // start = i, max_len = g+1;
+//                             dp[i][j] = true;
+//                         } else {
+//                             dp[i][j] = false;
+//                         }
+//                     } else {
+//                         dp[i][j] = false;
+//                     }
                 }
-            }    
-        }   
-        
-        for(int gap=2; gap<size; gap++) 
-        {
-            for(int i=0, j=gap; j<size; j++, i++) 
-            {
-                dp[i][j] = s.at(i)==s.at(j) && dp[i+1][j-1];
                 
-                if(dp[i][j])
-                {
-                    min_start = i;
-                    max_len = abs(j-i+1);
+                if(dp[i][j] == true) {
+                    start = i, max_len = abs(j-i+1);
                 }
             }
         }
         
-        return s.substr(min_start, max_len);
+        return s.substr(start, max_len);
     }
 };
