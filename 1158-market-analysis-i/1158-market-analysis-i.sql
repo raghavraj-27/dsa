4216f9cad07b9a04e9b246d2224902c5881b7b87
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-06-16 13:27:11 +0530

    Time: 1244 ms (46.76%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,6 +2,5 @@
 select U.user_id as buyer_id, U.join_date, if(count(O.order_date) is null, 0, count(O.order_date)) as orders_in_2019
 from Users as U
 left join Orders as O
-on O.buyer_id = U.user_id
-and YEAR(O.order_date) = '2019' 
+on O.buyer_id = U.user_id and YEAR(O.order_date) = '2019' 
 group by user_id
