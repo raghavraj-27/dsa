76a18a84236be6d64e9f4080786f51fc3987286b
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-06-21 18:00:01 +0530

    Time: 46 ms (5.54%), Space: 11.3 MB (5.20%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,53 +1,31 @@
 class Solution {
 public:
-    // TC : n! * n SC : O(n)
-//     void createPermutations(vector<int>& nums, vector<int> &v, vector<vector<int>>& ans,
-//                            map<int, bool> & freq)
-//     {
-//         if(v.size()>=nums.size()) {
-//             ans.push_back(v); 
-//             return;
-//         }
-        
-//         for(int ind=0; ind<nums.size(); ind++) {
-//             if(freq[ind] == false) {
-//                 freq[ind] = true; 
-//                 v.push_back(nums[ind]); 
-//                 createPermutations(nums, v, ans, freq); 
-//                 freq[ind] = false; 
-//                 v.pop_back();
-//             }
-//         }
-//     }
-    
-    
-    // TC : O(n! x n) SC : O(1)
-    void createPermutations(int ind, vector<int> & nums, vector<vector<int>> & ans)
-    {
-        if(ind >= nums.size()) {
-            ans.push_back(nums); 
+    void createPermutations(vector<int>& nums, map<int, bool> freq, vector<int> v, 
+                           vector<vector<int>>& ans) {
+        if(v.size() >= nums.size()) {
+            ans.push_back(v);
             return;
         }
         
-        for(int i=ind; i<nums.size(); i++) {
-            swap(nums[i], nums[ind]); 
-            createPermutations(ind+1, nums, ans); 
-            swap(nums[i], nums[ind]);
+        for(int i=0; i<nums.size(); i++) {
+            if(freq[nums[i]] == false) {
+                freq[nums[i]] = true;
+                v.push_back(nums[i]);
+                createPermutations(nums, freq, v, ans);
+                v.pop_back();
+                freq[nums[i]] = false;
+            }
         }
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans; 
-//         vector<int> v; 
-//         map<int, bool> freq;
+        map<int, bool> freq;
+        for(int i=0; i<nums.size(); i++) {
+            freq[nums[i]] = false;
+        }
         
-//         for(int i=0; i<nums.size(); i++) {
-//             freq[i] = false;
-//         }
-        
-        // createPermutations(nums, v, ans, freq);         
-        createPermutations(0, nums, ans); 
-
+        vector<int> v;
+        vector<vector<int>> ans;
+        createPermutations(nums, freq, v, ans);
         return ans;
     }
 };
