115d23fa6508e616ea2d14916c8a57df5fe2794d
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-06-21 16:28:02 +0530

    Time: 351 ms (16.63%), Space: 167.2 MB (11.65%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-vector<vector<string>> ans; 
-
-bool checkPalindrome(int start, int end, string str) {
-    while(start <= end)
-        if(str[start++] != str[end--])
-            return false;
-    return true;
-}
-
-void makePartition(int ind, string s, vector<string>& v) 
-{
-    if(ind>=s.size()) {
-        ans.push_back(v);
+    vector<vector<string>> ans;
+    
+    bool isPalindromic(int left, int right, string s) {
+        while(left < right) {
+            if(s[left++] != s[right--]) return false;
+        }
+        return true;
     }
-
-    for(int i=ind; i<s.size(); i++) 
-    {
-        if(checkPalindrome(ind, i, s))
-        {
-            string str = s.substr(ind, i-ind+1); 
-            // for(int p=ind; p<=i; p++)
-            //     str += s[p];
-            v.push_back(str); 
-            makePartition(i+1, s, v); 
-            v.pop_back();
+    
+    void makePatitions(int ind, string& s, vector<string> v) {
+        if(ind >= s.size()) {
+            ans.push_back(v);
+        }
+        
+        for(int i=ind; i<s.size(); i++) {
+            if(isPalindromic(ind, i, s)) {
+                string str = s.substr(ind, i-ind+1);
+                v.push_back(str);
+                makePatitions(i+1, s, v);
+                v.pop_back();
+            }
         }
     }
-}
-
-vector<vector<string>> partition(string s) {
-    vector<string> v;
-    makePartition(0, s, v);
-    return ans;
-}
+    
+    vector<vector<string>> partition(string s) {
+        vector<string> v;
+        makePatitions(0, s, v);
+        return ans;
+    }
 };
