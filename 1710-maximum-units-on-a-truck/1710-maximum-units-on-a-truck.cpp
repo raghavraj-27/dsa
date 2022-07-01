3047482445f7f04c6943bd4e2fcb101cb7d5b9be
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-01 10:01:38 +0530

    Time: 263 ms (19.75%), Space: 49.3 MB (20.23%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     static bool cmp(vector<int> v1, vector<int> v2) {
-//         double val1 = (double)v1[1] / v1[0];
-//         double val2 = (double)v2[1] / v2[0];
-        
-//         if(val1 > val2) return true;
-//         return false;
-//         if(v1[0]*v1[1] > v2[0]*v2[1]) {
-//             return true;
-//         }
-//         if(v1[0]*v1[1] == v2[0]*v2[1] and v1[0] < v2[0]) {
-//             return true;
-//         }
         if(v1[1] > v2[1]) {
             return true;
         }
@@ -20,10 +9,6 @@ public:
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         sort(boxTypes.begin(), boxTypes.end(), cmp);
-        for(auto v : boxTypes) {
-            cout<<"["<<v[0]<<","<<v[1]<<"] ";
-        }
-        cout<<endl;
         int ans = 0;
         
         for(int i=0; i<boxTypes.size(); i++) {
