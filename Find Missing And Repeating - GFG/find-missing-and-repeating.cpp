f53fbcdfe582350420bd2a534fbeed7ddf23ea56
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-03 17:29:23 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -7,34 +7,18 @@ using namespace std;
 class Solution{
 public:
     int *findTwoElement(int *arr, int n) {
-        long long s = ((long long)(1LL*n*(n+1))) / 2;
-        long long s2 = ((long long)(1LL*n*(n+1)*(2*n+1))) / 6;
-        
-        long long S = 0, S2 = 0;
-        for(int i=0; i<n; i++) {
-            S += arr[i];
-            S2 += ((long long)(1LL*arr[i]*arr[i]));
-        }
-        
-        long long val1 = S - s;
-        long long val2 = S2 - s2;
-        long long val3 = val2 / val1;
-        
-        long long ans1 = (val1 + val3) / 2;
-        long long ans2 = ans1 - val1;
-        
         int *ans = new int[2];
-        for(int i=0; i<n; i++) {
-            if(arr[i] == ans1) {
-                ans[0] = ans1; 
-                ans[1] = ans2;
-                return ans;
-            }
+        long sum=0,sum2=0;
+        for(int i=0;i<n;i++){
+            sum+=(arr[i]-(i+1));
+            sum2+=(1LL*arr[i]*arr[i] - 1LL*(i+1)*(i+1));
         }
-        
-        ans[0] = ans2; 
-        ans[1] = ans1;
-        
+        //sum2 = a*a-b*b = (a-b)(a+b) = (sum)(a+b)
+        //sum2/sum = a+b
+        //sum = a-b
+        //hence
+        ans[0] =  (sum2/sum + sum) / 2;
+        ans[1] =  (sum2/sum - sum) / 2;
         return ans;
     }
 };
