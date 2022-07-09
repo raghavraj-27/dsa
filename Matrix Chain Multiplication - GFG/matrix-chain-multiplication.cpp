55ffd6fab4c7cbd5966b7e4b98006ff008cafccb
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-09 13:06:11 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int solve(int i, int j, int arr[], vector<vector<int>>& dp) {
+        if(i == j) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int res = INT_MAX;
+        for(int k=i; k<j; k++) {
+            int opers = (arr[i-1] * arr[k] * arr[j]) + solve(i,k,arr,dp) + solve(k+1,j,arr, dp);
+            res = min(res, opers);
+        }
+        
+        return dp[i][j] = res;
+    }
+
+    int matrixMultiplication(int N, int arr[]) {
+        vector<vector<int>> dp(N+1, vector<int> (N+1, -1));
+        return solve(1, N-1, arr, dp);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
