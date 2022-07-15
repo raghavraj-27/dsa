48cb733029afe12b7440bd2c50dac2330fcec3d4
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-15 12:24:11 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (37.78%) - LeetHub

--- 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.cpp
+++ 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-int f(int i, int j, string& a, string& b) {
-    if(i < 0 or j < 0) return 0;
-    if(a[i] != b[j]) return 1 + f(i-1, j-1, a, b);
-    return max(f(i-1, j, a, b), f(i, j-1, a, b));
-}
-int findLUSlength(string a, string b) {
-    if(a == b) return -1;
-    return max(a.size(), b.size());
-    // cout<<a.size()<<endl;
-//     if(a.size() != b.size()) return max(a.size(), b.size());
-//     if(a.size() == b.size() and a!=b) return a.size();
-//     int ans = f(a.size()-1, b.size()-1, a, b);
-//     if(ans == 0) return -1;
-//     return ans;
-// }
-}
+    int findLUSlength(string a, string b) {
+        return a==b ? -1 : max(a.size(), b.size());
+    }
 };
