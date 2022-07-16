9fe3fb4693357cf1a6cf5065d721a6ec0334e917
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-16 09:46:41 +0530

    Time: 35 ms (9.53%), Space: 13.5 MB (5.50%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,29 +1,8 @@
 class Solution {
 public:
-    void convert(string& ans, int num, vector<int>& v, map<int, string>& mp) {
-        if(num == 0) return;
-        if(num < 4) {
-            for(int i=0; i<num; i++) {
-                ans+="I";
-            }
-            return;
-        }
-
-        int ind = lower_bound(v.begin(), v.end(), num) - v.begin();
-        // if(ind == v.size()) {
-        //     cout<<ind<<endl;
-        //     ind--;
-        // }
-        if( ind == v.size() or v[ind] > num) {
-            ind--;
-        }
-
-        ans += mp[v[ind]];
-        convert(ans, num - v[ind], v, mp);
-    }
-
-    string intToRoman(int num) {
-        map<int, string> mp;
+    map<int, string> mp;
+    vector<int> v;
+    Solution() {
         mp[1] = "I";        
         mp[5] = "V";
         mp[10] = "X";
@@ -38,11 +17,28 @@ public:
         mp[400] = "CD";
         mp[900] = "CM";
 
-        vector<int> v = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-        sort(v.begin(), v.end());
+        v = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    }
+    void convert(string& ans, int num) {
+        if(num < 4) {
+            for(int i=0; i<num; i++) {
+                ans+="I";
+            }
+            return;
+        }
 
+        int ind = lower_bound(v.begin(), v.end(), num) - v.begin();
+        if( ind == v.size() or v[ind] > num) {
+            ind--;
+        }
+
+        ans += mp[v[ind]];
+        convert(ans, num - v[ind]);
+    }
+
+    string intToRoman(int num) {
         string ans = "";
-        convert(ans, num, v, mp);
+        convert(ans, num);
         return ans;
     }
 };
