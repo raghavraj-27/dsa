23272fedd161f475d6250a6f7525d8882f33edfa
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-16 15:58:35 +0530

    Time: 27 ms (27.61%), Space: 6.5 MB (90.53%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    
+    int f(int i, int j, int m, int n, int maxMove, int dp[51][51][51]) {
+        if(maxMove < 0) return 0;
+        
+        if(i<0 or i>=m or j<0 or j>=n) {
+            return 1;
+        }
+        
+        
+        if(dp[i][j][maxMove] >= 0) return dp[i][j][maxMove];
+        
+        int a = f(i+1, j, m, n, maxMove-1, dp);
+        int b = f(i-1, j, m, n, maxMove-1, dp);
+        int c = f(i, j+1, m, n, maxMove-1, dp);
+        int d = f(i, j-1, m, n, maxMove-1, dp);
+
+        return dp[i][j][maxMove] = ((a + b)%mod + (c + d)%mod) % mod;
+    }
+    
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int dp[51][51][51];
+        for(int i=0; i<51; i++) {
+            for(int j=0; j<51; j++) {
+                for(int k=0; k<51; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return f(startRow, startColumn, m, n, maxMove, dp);
+    }
+};
