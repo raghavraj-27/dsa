09d06a5510788c5e62fb31e4e03b8570d60d9cce
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-17 12:14:19 +0530

    Time: 2813 ms (11.11%), Space: 421 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    static bool cmp(pair<string, int>& p1, pair<string, int>& p2) {
-        if(p1.first == p2.first) return p1.second < p2.second;
-        return p1.first < p2.first;
-    }
     int getKth(vector<string>& nums, int k, int t) {
         vector<pair<string, int>> v;
         
