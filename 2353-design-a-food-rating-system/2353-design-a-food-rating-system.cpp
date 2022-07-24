3ebdbe7c3ebdfeb1b59d8f7f5b808e651445e3f6
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-24 13:48:50 +0530

    Time: 1114 ms (33.33%), Space: 159.3 MB (33.33%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -1,25 +1,42 @@
+struct comp{
+    bool operator()(pair<int,string>a,pair<int,string>b)const{
+        if(a.first!=b.first)
+            return a.first>b.first;
+        return a.second<b.second;
+    }
+};
 class FoodRatings {
+    /*static bool comp(pair<int,string>a,pair<int,string>b){
+        if(a.first!=b.first)
+            return a.first>b.first;
+        return a.second<b.second;
+    }*/
 public:
-    map<string, string> f_to_c;
-    map<string, int> f_to_r;
-    map<string, set<pair<int, string>>> mp;
-    
+    vector<string>f,c;
+    vector<int>r;
+    int n,i;
+    map<string,set<pair<int,string>,comp>>m;
+    map<string,int>name;
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for(int i=0; i<foods.size(); i++) {
-            f_to_c[foods[i]] = cuisines[i];
-            f_to_r[foods[i]] = ratings[i];
-            mp[cuisines[i]].insert({-ratings[i], foods[i]});
+        f=foods;c=cuisines;r=ratings;
+        n=foods.size();
+        for(i=0; i<n; i++){
+            m[c[i]].insert({r[i],f[i]});
+            name[f[i]]=i;
         }
     }
     
-    void changeRating(string food, int newRating) {
-        string cuisine = f_to_c[food];
-        mp[cuisine].erase({-f_to_r[food], food});
-        mp[cuisine].insert({-newRating, food});
-        f_to_r[food] = newRating;
+    void changeRating(string food, int nr) {
+        string cu=c[name[food]];
+        int old=r[name[food]];
+        r[name[food]]=nr;
+        m[cu].erase({old,food});
+        m[cu].insert({nr,food});
     }
     
-    string highestRated(string cuisine) {
-        return mp[cuisine].begin()->second;
+    string highestRated(string c) {
+        for(auto it:m[c])
+            return it.second;
+        return "";
     }
 };
