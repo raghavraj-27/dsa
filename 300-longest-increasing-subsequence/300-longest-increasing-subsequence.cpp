b5043e0a3abef93d3187e7ee2efb5e27ae2212e3
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-07-26 20:21:37 +0530

    Time: 8 ms (94.83%), Space: 10.4 MB (67.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        // DP Approach
-        /*
-        vector<int> dp(nums.size(), 0); 
-        dp[0] = 1; 
+//         vector<int> v; 
+//         v.push_back(nums[0]);
         
-        for(int i=1; i<nums.size(); i++) {
-            int prev = INT_MIN; 
-            for(int j=0; j<i; j++) {
-                if(nums[j] < nums[i] and dp[j] > prev) {
-                    dp[i] = dp[j]; 
-                    prev = dp[j];
-                }
-            } 
-            dp[i]++;
-        }
+//         for(int i=1; i<nums.size(); i++) {
+//             if(v.back() < nums[i]) {
+//                 nums.push_back(nums[i]);
+//             } else {
+//                 int ind = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+//                 v[ind] = nums[i];
+//             }
+//         }
         
-        return *max_element(dp.begin(), dp.end());
-        */
-        
-        vector<int> seq; 
-        seq.push_back(nums[0]); 
-        for(int i=1; i<nums.size(); i++) {
-            if(nums[i] > seq.back()) 
-                seq.push_back(nums[i]);
-            else {
-                int index = lower_bound(seq.begin(), seq.end(), nums[i]) - seq.begin();
-                seq[index] = nums[i];
+//         return v.size();
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the smallest number >= x
+                *it = x; // Replace that number with x
             }
         }
-        
-        return seq.size();
+        return sub.size();
     }
 };
