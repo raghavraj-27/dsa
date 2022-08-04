98709162c80c079cbb2c049a1aa90448839cb0c5
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-08-04 11:34:15 +0530

    Time: 92 ms (68.81%), Space: 10.4 MB (39.42%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -26,8 +26,26 @@ public:
         // for making sub problems independent,
         // once you make recursive tree you will understand this use cases
         sort(cuts.begin(), cuts.end());
-        vector<vector<int>> dp(cuts.size(), vector<int> (cuts.size(), -1));
+        vector<vector<int>> dp(cuts.size(), vector<int> (cuts.size(), 0));
         
-        return f(1, c, cuts, dp);
+        for(int i=c; i>=1; i--) {
+            for(int j=1; j<=c; j++) {
+                if(i > j) continue;
+                
+                int res = INT_MAX;        
+                for(int ind=i; ind<=j; ind++) {
+                    int len = cuts[j+1] - cuts[i-1];
+                    int left = dp[i][ind-1];
+                    int right = dp[ind+1][j];
+
+                    int cost = len + left + right;
+                    res = min(cost, res);
+                }
+
+                dp[i][j] = res;
+            }
+        }
+        
+        return dp[1][c];
     }
 };
