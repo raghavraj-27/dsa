ad1e64cff463a9eb22d8ac5234c6684afe51c351
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-08-04 21:54:47 +0530

    Time: 0 ms (100.00%), Space: 17.2 MB (97.94%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -11,97 +11,37 @@
  */
 class Solution {
 public:
-     int widthOfBinaryTree(TreeNode* root) {
-        vector<int> leftmost, rightmost, leftmost_parent;
-        stack<tuple<TreeNode*, int, int>> nodes;
-        nodes.push(make_tuple(root, 0, 0));
-        int fixed_index;
-        
-        
-        while(!nodes.empty()){
-            auto node = nodes.top();
-            nodes.pop();
-            
-            TreeNode* cur = get<0>(node);
-            int index=get<1>(node), depth = get<2>(node);
-            
-            if (depth == leftmost.size()){
-                leftmost.push_back(index);
-                rightmost.push_back(index);
-            }
-            else{
-                rightmost[depth] = index;
-            }
-            
-            if(cur->right || cur->left){
-                
-                if(depth == leftmost_parent.size()){
-                    leftmost_parent.push_back(index);
-                }
-                fixed_index = index-leftmost_parent[depth];
-                
-            }
-            
-            if(cur->right){
-                nodes.push(make_tuple(cur->right, 2*fixed_index+1, depth+1));
-            }
-            
-            if(cur->left){
-                nodes.push(make_tuple(cur->left, 2*fixed_index, depth+1));
-            }
-            
-        }
-        
-        int max_width = 0;
-        
-        for(int i=0;i<leftmost.size();i++){
-            max_width = max(rightmost[i]-leftmost[i]+1, max_width);
-        }
-        return max_width;
-    }
-
-    /*
-    void setTheTree(TreeNode* root) {
-        if(root == NULL) return;
-        if(root->left == NULL and root->right == NULL) return;
-        
-        if(root->left == NULL) {
-            root->left = new TreeNode(1000);
-            setTheTree(root->right);
-            return;
-        }
-        
-        if(root->right == NULL) {
-            root->right = new TreeNode(1000);
-            setTheTree(root->left);
-            return;
-        }
-        
-        setTheTree(root->left);
-        setTheTree(root->right);
-    }  
-    int findHeight(TreeNode* root) {
-        if(root == NULL) return 0;
-        return max(findHeight(root->left), findHeight(root->right)) + 1;
-    }
-    int calcLevel(TreeNode* root, int h, int nodes) {
-        if(root == NULL) return 0;
-        if(h == 1) return 1;
-        int a = calcLevel(root->left, h-1, nodes);
-        int b = calcLevel(root->right, h-1, nodes);
-        return a + b;
-    }
     int widthOfBinaryTree(TreeNode* root) {
-        int height = findHeight(root);
-        setTheTree(root);
+        if(root == nullptr) return 0;
         
-        int ans = INT_MIN;
-        for(int i=1; i<=height; i++) {
-            // vector<int> v = calcLevel(root, i);
-            ans = max(ans, calcLevel(root, i, 0));
+        int width = 0;
+        queue<pair<TreeNode*, long long>> pq;
+        pq.push({root, 0});
+        
+        while(not pq.empty()) {
+            int sz = pq.size();
+            int mn = pq.front().second;
+            
+            int first, last;
+            for(int i=0; i<sz; i++) {
+                long long ind = pq.front().second - mn;
+                TreeNode* curr = pq.front().first;
+                pq.pop();
+                
+                if(i==0) first = ind;
+                if(i==sz-1) last = ind;
+                
+                if(curr->left != nullptr) {
+                    pq.push({curr->left, 2*ind + 1});
+                }
+                if(curr->right != nullptr) {
+                    pq.push({curr->right, 2*ind + 2});
+                }
+            }
+            
+            width = max(width, last - first + 1);
         }
         
-        return ans;
+        return width;
     }
-    */
 };
