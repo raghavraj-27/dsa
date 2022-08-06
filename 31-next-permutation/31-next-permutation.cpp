6925a673fbf64883977428be01dbd7455d850d0f
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-08-06 23:12:43 +0530

    Time: 9 ms (34.33%), Space: 12.2 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -3,36 +3,42 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size(); 
         int ind = -1, diff = INT_MAX; 
-        for(int i=n-2; i>=0; i--)
-        {
-            if(nums[i] < nums[i+1])
-            {
+        for(int i=n-2; i>=0; i--) {
+            if(nums[i] < nums[i+1]) {
                 ind = i; 
                 break;
             }
         }
-        
+
         if(ind == -1) {
             reverse(nums.begin(), nums.end()); 
             return;
         }
-        
-        int ind2;
+        int jnd = 0;
         for(int i=n-1; i>=0; i--) {
-            if(nums[i] > nums[ind] and abs(nums[i] - nums[ind]) < diff) {
-                ind2 = i;
+            if(nums[i] > nums[ind]) {
+                jnd = i;
                 break;
             }
         }
-        
-        int temp = nums[ind]; 
-        nums[ind] = nums[ind2]; 
-        nums[ind2] = temp;
-        
-        sort(nums.begin()+ind+1, nums.end());
-        // auto it = nums.begin(); 
-        // advance(it, ind+1);
-        // reverse(it, nums.end()); 
-        return;
+        swap(nums[ind], nums[jnd]);
+        reverse(nums.begin()+ind+1, nums.end());
+//         int ind2;
+//         for(int i=n-1; i>=0; i--) {
+//             if(nums[i] > nums[ind] and abs(nums[i] - nums[ind]) < diff) {
+//                 ind2 = i;
+//                 break;
+//             }
+//         }
+
+//         swap(nums[ind], nums[ind2]);
+
+        // After swaping, you can either sort the array after ind or you can reverse the array after ind
+        // sorting will cause TC = O(n) + O(n) + O(n.log n) = O(n. log n)
+        // reverse will cause TC = O(n) + O(n) + O(n) = O(n)
+        // This is why revering is better approach here
+
+        // sort(nums.begin()+ind+1, nums.end());
+        // reverse(nums.begin()+ind+1, nums.end());
     }
 };
