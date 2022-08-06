65c040c1aeab5a6781dd6dcc486b4fccc61a7787
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-08-06 23:13:42 +0530

    Time: 19 ms (5.84%), Space: 12.2 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -23,22 +23,5 @@ public:
         }
         swap(nums[ind], nums[jnd]);
         reverse(nums.begin()+ind+1, nums.end());
-//         int ind2;
-//         for(int i=n-1; i>=0; i--) {
-//             if(nums[i] > nums[ind] and abs(nums[i] - nums[ind]) < diff) {
-//                 ind2 = i;
-//                 break;
-//             }
-//         }
-
-//         swap(nums[ind], nums[ind2]);
-
-        // After swaping, you can either sort the array after ind or you can reverse the array after ind
-        // sorting will cause TC = O(n) + O(n) + O(n.log n) = O(n. log n)
-        // reverse will cause TC = O(n) + O(n) + O(n) = O(n)
-        // This is why revering is better approach here
-
-        // sort(nums.begin()+ind+1, nums.end());
-        // reverse(nums.begin()+ind+1, nums.end());
     }
 };
