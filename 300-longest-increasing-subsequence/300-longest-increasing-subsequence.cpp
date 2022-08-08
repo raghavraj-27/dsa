a129f375338f9658b4129c5f443622f0ef544ac0
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-08-08 15:35:42 +0530

    Time: 373 ms (56.04%), Space: 10.6 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-//         vector<int> v; 
-//         v.push_back(nums[0]);
+        // i -> max len of longest increasing subsequence
+        // i=0 -> 1
+        // i=1, if(nums[i-1] < nums[i]) => 2
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int ans = 1;
         
-//         for(int i=1; i<nums.size(); i++) {
-//             if(v.back() < nums[i]) {
-//                 nums.push_back(nums[i]);
-//             } else {
-//                 int ind = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
-//                 v[ind] = nums[i];
-//             }
-//         }
-        
-//         return v.size();
-        vector<int> sub;
-        for (int x : nums) {
-            if (sub.empty() || sub[sub.size() - 1] < x) {
-                sub.push_back(x);
-            } else {
-                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the smallest number >= x
-                *it = x; // Replace that number with x
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                } 
             }
+            ans = max(ans, dp[i]);
         }
-        return sub.size();
+        
+        return ans;
     }
 };
