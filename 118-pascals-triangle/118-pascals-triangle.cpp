d278105d9f94dafaa0b6fcc52f16686fc8a2b9ff
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-09-23 11:12:40 +0530

    Time: 4 ms (25.95%), Space: 6.3 MB (90.64%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> f(int r) {
+        vector<int> v(r, 1);
+        
+        int N = r-1;
+        for(int i=1; i<N; i++) {
+            int val = 1;
+            for(int j=0; j<i; j++) {
+                val *= (N-j);
+                val /= (j+1);
+            }
+            
+            v[i] = val;
+        }
+        
+        return v;
+    }
+    
+    // 10C7 = 8.9.10
+    //        ______
+    //        1.2.3
+    
+    vector<vector<int>> generate(int n) {
+        // n = row
+        //     => nth row will have n elements
+        //     if n=1 ,      1                  0C0     
+        //     if n=2,      1 1               0C0 0C0   
+        //     if n=3,     1 2 1            2C0 2C1 2C2
+        //     if n=4,    1 3 3 1         3C0 3C1 3C2 3C3
+        vector<vector<int>> ans;
+        if(n >= 1) {
+            ans.push_back({1});
+        }
+        if(n >= 2) {
+            ans.push_back({1, 1});
+        }
+        
+        for(int i=3; i<=n; i++) {
+            ans.push_back(f(i));
+        }
+        
+        return ans;
+    }
+};
