7671260f3b2dac15504471890758c424e9e0eb80
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-12-10 22:50:11 +0530

    Time: 15 ms (38.61%), Space: 10.8 MB (37.09%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -8,19 +8,16 @@ public:
             ans.push_back(v);
             return;
         }
-        if(i >= N or sum > target) return;
         
         for(int ind=i; ind<c.size(); ind++) {
             if(ind!=i and c[ind] == c[ind-1]) continue;
+            
+            if(sum + c[ind] > target) break;
+            
             v.push_back(c[ind]);
             f(ind+1, N, c, sum + c[ind], target);
             v.pop_back();
-            // f(ind+1, N, c, sum, target);
         }
-        // v.push_back(c[i]);
-        // f(i+1, N, c, sum + c[i], target);
-        // v.pop_back();
-        // f(i+1, N, c, sum, target);
     } 
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
