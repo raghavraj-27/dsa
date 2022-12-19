d3536cc7796621fc676ff440bc5af2666d93075f
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2022-12-19 18:52:24 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -1,43 +1,37 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution{
     public:
-    int kthElement(int arr1[], int arr2[], int n, int m, int k)
-    {
-        if(n > m) {
-            return kthElement(arr2, arr1, m, n, k);
-        }
+    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        if(n > m) return kthElement(arr2, arr1, m, n, k);
         
-        int low = max(0, k-m), high = min(n, k);
+        int low = max(0, k-m), high = min(k, n);
         while(low <= high) {
-            int cut1 = (low + high) / 2;
+            int cut1 = (low + high) >> 1;
             int cut2 = k - cut1;
             
-            int left1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1];
-            int left2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
-            
+            int left1 = cut1 == 0 ? INT_MIN : arr1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : arr2[cut2-1];
             int right1 = cut1 == n ? INT_MAX : arr1[cut1];
             int right2 = cut2 == m ? INT_MAX : arr2[cut2];
             
             if(left1 <= right2 and left2 <= right1) {
                 return max(left1, left2);
-            } 
-            else if(left1 > right2) {
-                high = cut1 - 1;
+            } else if(left1 > right2) {
+                high = cut1-1;
             } else {
-                low = cut1 + 1;
+                low = cut1+1;
             }
         }
-        
         return 0;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
  
 // Driver code
 int main()
@@ -57,4 +51,5 @@ int main()
         cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
 	}
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
