256a6df0ee5c824b112188c3a5ba3e0eb29a5ff7
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2023-02-18 10:42:18 +0530

    Time: 24 ms (45.09%), Space: 15.3 MB (50.55%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    
+    vector<int> search(vector<pair<int, int>> adj[], int n) {
+        vector<int> dist(n, -1);
+        dist[0] = 0;
+        queue<pair<int, pair<int, int>>> pq;
+        pq.push({0, {0, -1}});
+        
+        vector<vector<int>> vis(n, vector<int> (2, 0));
+        vis[0][0] = 1;
+        vis[0][1] = 1;
+        
+        while(not pq.empty()) {
+            int currDist = pq.front().first;
+            int currNode = pq.front().second.first;
+            int prevColor = pq.front().second.second;
+            pq.pop();
+            
+            for(auto p: adj[currNode]) {
+                int nextNode = p.first;
+                int color = p.second;
+                
+                if(vis[nextNode][color] == 0 and prevColor != color) {
+                    if(dist[nextNode] == -1)
+                        dist[nextNode] = currDist + 1;
+                    pq.push({currDist + 1, {nextNode, color}});
+                    vis[nextNode][color] = 1;
+                }
+            }
+        }
+        
+        return dist;
+    }
+    
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<pair<int, int>> adj[n];
+        // vector<int> badj[n];
+        
+        for(auto v: redEdges) {
+            adj[v[0]].push_back({v[1], 0});
+        }
+        for(auto v: blueEdges) {
+            adj[v[0]].push_back({v[1], 1});
+        }
+        
+        // dist.resize(n, -1);
+        // for(int i=0; i<n; i++) {
+        //     dist[i] = INT_MAX;
+        // }
+        return search(adj, n);
+        // for(int i=0; i<n; i++) {
+        //     if(dist[i] == INT_MAX) {
+        //         dist[i] = -1;
+        //     }
+        // }
+        
+    }
+};
