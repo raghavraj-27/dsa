f36c17c81b23398c42f1b8c16da540ab7f653e3e
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2023-05-20 18:26:11 +0530

    Time: 286 ms (36.59%), Space: 128.1 MB (45.06%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& vis, int& cntNode, int& cntEdge) {
+        vis[node] = true;
+        cntNode++;
+        
+        for(int ele: adj[node]) {
+            cntEdge++;
+            if(vis[ele] == false) {
+                
+                dfs(ele, adj, vis, cntNode, cntEdge);
+            }
+        }
+    }
+//     bool isCompleteComponent(int node, vector<vector<int>>& adj) {
+        
+//     }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        
+        for(vector<int> v: edges) {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        
+        // vector<int> components;
+        vector<bool> vis(n, false);
+        int cnt = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(vis[i] == false) {
+                int cntNode = 0, cntEdge = 0;
+                dfs(i, adj, vis, cntNode, cntEdge);
+                // cout<<i<<" - NodeOfNodes = "<<cntNode<<" NoOfEdges = "<<cntEdge<<"\n";
+                if(cntEdge / 2 == cntNode*(cntNode - 1) / 2) {
+                    
+                    cnt++;
+                }
+                // components.push_back(i);
+            }
+        }
+        cout<<"\n";
+        
+//         for(int node: components) {
+//             if(isCompleteComponent(node, adj)) {
+//                 cnt++;
+//             }
+//         }
+        
+        return cnt;
+    }
+};
