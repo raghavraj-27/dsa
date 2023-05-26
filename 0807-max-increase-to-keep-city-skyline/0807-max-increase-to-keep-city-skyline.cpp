3ab512c547875a7376246a264093c2c4ad16afe3
Raghvendra Pd Sr <73431762+raghavraj-27@users.noreply.github.com>
2023-05-26 13:47:17 +0530

    Time: 8 ms (64.74%), Space: 10.5 MB (5.17%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -11,35 +11,19 @@ public:
                 colMax[i] = max(colMax[i], grid[j][i]);
             }
         }
-        // for(int i=0; i<n; i++) {
-        //     for(int j=0; j<n; j++) {
-        //         colMax[i] = max(colMax[i], grid[j][i]);
-        //     }
-        // }
-        // cout<<"Row Max - \n";
-        // for(int i=0; i<n; i++) cout<<rowMax[i]<<"\n";
-        // cout<<"\nCol Max - \n";
-        // for(int i=0; i<n; i++) cout<<colMax[i]<< " ";
-        // cout<<"\n";
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(myGrid[i][j] == rowMax[i] or myGrid[i][j] == colMax[j]) continue;
                 myGrid[i][j] = min(rowMax[i], colMax[j]);
-                
-                
             }
-            
         }
-        
         
         int ans = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                // cout<<myGrid[i][j]<<" ";
                 ans += myGrid[i][j] - grid[i][j];
             }
-            // cout<<"\n";
         }
         
         return ans;
